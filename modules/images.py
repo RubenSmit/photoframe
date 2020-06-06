c99e6a5fa5cdc2907d2a5ee644eba3faba020578
Ruben Smit <rubensmit@live.nl>
2020-06-06 15:49:49 +0200

    Add textwrapper to description

--- modules/images.py
+++ modules/images.py
@@ -14,6 +14,7 @@
 # along with photoframe.  If not, see <http://www.gnu.org/licenses/>.
 #
 import hashlib
+import textwrap
 
 class ImageHolder:
   def __init__(self):
@@ -62,7 +63,7 @@ class ImageHolder:
     return self
 
   def setDescription(self, description):
-    self.description = description
+    self.description = TextWrapper().wrap(description)
     return self
 
   def setDimensions(self, width, height):
