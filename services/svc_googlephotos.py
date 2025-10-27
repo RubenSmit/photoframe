c547672fec37d7fccdcc735db1477f9f51eb5e6c
Ruben Smit <rubensmit@live.nl>
2025-10-27 12:43:20 +0100

    Replace the google photos service with a new one

--- services/svc_googlephotos.py
+++ services/svc_googlephotos.py
@@ -14,319 +14,125 @@
 # along with photoframe.  If not, see <http://www.gnu.org/licenses/>.
 #
 from base import BaseService
-import os
-import json
 import logging
-from modules.network import RequestResult
+import re
+from urllib2 import urlopen
+from urlparse import urljoin
+
+from modules.helper import helper
 
 class GooglePhotos(BaseService):
-  SERVICE_NAME = 'GooglePhotos'
-  SERVICE_ID = 2
+  SERVICE_NAME = 'Google Photos'
+  SERVICE_ID   = 5
 
   def __init__(self, configDir, id, name):
-    BaseService.__init__(self, configDir, id, name, needConfig=False, needOAuth=True)
+    BaseService.__init__(self, configDir, id, name, needConfig=False, needOAuth=False)
 
-  def getOAuthScope(self):
-    return ['https://www.googleapis.com/auth/photoslibrary.readonly']
-
-  def helpOAuthConfig(self):
-    return 'Please upload client.json from the Google API Console'
+    self.brokenUrls = []
 
   def helpKeywords(self):
-    return 'Currently, each entry represents the name of an album (case-insensitive). If you want the latest photos, simply write "latest" as album'
+    return 'Each item is a URL to a public google photos album.'
+
+  def removeKeywords(self, index):
+    url = self.getKeywords()[index]
+    result = BaseService.removeKeywords(self, index)
+    if result and url in self.brokenUrls:
+      self.brokenUrls.remove(url)
+    return result
 
   def hasKeywordSourceUrl(self):
     return True
-
-  def getExtras(self):
-    # Normalize
-    result = BaseService.getExtras(self)
-    if result is None:
-      return {}
-    return result
-
-  def postSetup(self):
-    extras = self.getExtras()
-    keywords = self.getKeywords()
-
-    if len(extras) == 0 and keywords is not None and len(keywords) > 0:
-      logging.info('Migrating to new format with preresolved album ids')
-      for key in keywords:
-        if key.lower() == 'latest':
-          continue
-        albumId = self.translateKeywordToId(key)
-        if albumId is None:
-          logging.error('Existing keyword cannot be resolved')
-        else:
-          extras[key] = albumId
-      self.setExtras(extras)
-    else:
-      # Make sure all keywords are LOWER CASE (which is why I wrote it all in upper case :))
-      extras_old = self.getExtras()
-      extras = {}
-
-      for k in extras_old:
-        kk = k.upper().lower().strip()
-        if len(extras) > 0 or kk != k:
-          extras[kk] = extras_old[k]
-
-      if len(extras) > 0:
-        logging.debug('Had to translate non-lower-case keywords due to bug, should be a one-time thing')
-        self.setExtras(extras)
-
-      # Sanity, also make sure extras is BLANK if keywords is BLANK
-      if len(self.getKeywords()) == 0:
-        extras = self.getExtras()
-        if len(extras) > 0:
-          logging.warning('Mismatch between keywords and extras info, corrected')
-          self.setExtras({})
 
   def getKeywordSourceUrl(self, index):
     keys = self.getKeywords()
     if index < 0 or index >= len(keys):
       return 'Out of range, index = %d' % index
-    keywords = keys[index]
-    extras = self.getExtras()
-    if keywords not in extras:
-      return 'https://photos.google.com/'
-    return extras[keywords]['sourceUrl']
-
-  def removeKeywords(self, index):
-    # Override since we need to delete our private data
-    keys = self.getKeywords()
-    if index < 0 or index >= len(keys):
-      return
-    keywords = keys[index].upper().lower().strip()
-    filename = os.path.join(self.getStoragePath(), self.hashString(keywords) + '.json')
-    if os.path.exists(filename):
-      os.unlink(filename)
-    if BaseService.removeKeywords(self, index):
-      # Remove any extras
-      extras = self.getExtras()
-      if keywords in extras:
-        del extras[keywords]
-        self.setExtras(extras)
-      return True
-    else:
-      return False
+    return keys[index]
 
   def validateKeywords(self, keywords):
-    tst = BaseService.validateKeywords(self, keywords)
-    if tst["error"] is not None:
-      return tst
+    # Catches most invalid URLs
+    if not helper.isValidUrl(keywords):
+      return {'error': 'URL appears to be invalid', 'keywords': keywords}
 
-    # Remove quotes around keyword
-    if keywords[0] == '"' and keywords[-1] == '"':
-      keywords = keywords[1:-1]
-    keywords = keywords.upper().lower().strip()
+    return BaseService.validateKeywords(self, keywords)
 
-    # No error in input, resolve album now and provide it as extra data
-    albumId = None
-    if keywords != 'latest':
-      albumId = self.translateKeywordToId(keywords)
-      if albumId is None:
-        return {'error':'No such album "%s"' % keywords, 'keywords' : keywords}
+  def memoryForget(self, keywords=None, forgetHistory=False):
+    # give broken URLs another try (server may have been temporarily unavailable)
+    self.brokenUrls = []
+    return BaseService.memoryForget(self, keywords=keywords, forgetHistory=forgetHistory)
 
-    return {'error':None, 'keywords':keywords, 'extras' : albumId}
+  def getUrlFilename(self, url):
+    return url.rsplit("/", 1)[-1]
 
-  def addKeywords(self, keywords):
-    result = BaseService.addKeywords(self, keywords)
-    if result['error'] is None and result['extras'] is not None:
-      k = result['keywords']
-      extras = self.getExtras()
-      extras[k] = result['extras']
-      self.setExtras(extras)
-    return result
-
-
-  def isGooglePhotosEnabled(self):
-    url = 'https://photoslibrary.googleapis.com/v1/albums'
-    data = self.requestUrl(url, params={'pageSize':1})
-    '''
-{\n  "error": {\n    "code": 403,\n    "message": "Photos Library API has not been used in project 742138104895 before or it is disabled. Enable it by visiting https://console.developers.google.com/apis/api/photoslibrary.googleapis.com/overview?project=742138104895 then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.",\n    "status": "PERMISSION_DENIED",\n    "details": [\n      {\n        "@type": "type.googleapis.com/google.rpc.Help",\n        "links": [\n          {\n            "description": "Google developers console API activation",\n            "url": "https://console.developers.google.com/apis/api/photoslibrary.googleapis.com/overview?project=742138104895"\n          }\n        ]\n      }\n    ]\n  }\n}\n'
-    '''
-    return not (data.httpcode == 403 and 'Enable it by visiting' in data.content)
-
-  def getQueryForKeyword(self, keyword):
-    result = None
-    extras = self.getExtras()
-    if extras is None:
-      extras = {}
-
-    if keyword == 'latest':
-      logging.debug('Use latest 1000 images')
-      result = {
-        'pageSize' : 100, # 100 is API max
-        'filters': {
-          'mediaTypeFilter': {
-            'mediaTypes': [
-              'PHOTO'
-            ]
-          }
-        }
-      }
-    elif keyword in extras:
-      result = {
-        'pageSize' : 100, # 100 is API max
-        'albumId' : extras[keyword]['albumId']
-      }
-    return result
-
-  def translateKeywordToId(self, keyword):
-    albumid = None
-    source = None
-    albumname = None
-
-    if keyword == '':
-      logging.error('Cannot use blank album name')
-      return None
-
-    if keyword == 'latest':
-      return None
-
-    logging.debug('Query Google Photos for album named "%s"', keyword)
-    url = 'https://photoslibrary.googleapis.com/v1/albums'
-    params={'pageSize':50} #50 is api max
-    while True:
-      data = self.requestUrl(url, params=params)
-      if not data.isSuccess():
-        return None
-      data = json.loads(data.content)
-      for i in range(len(data['albums'])):
-        if 'title' in data['albums'][i]:
-          logging.debug('Album: %s' % data['albums'][i]['title'])
-        if 'title' in data['albums'][i] and data['albums'][i]['title'].upper().lower().strip() == keyword:
-          logging.debug('Found album: ' + repr(data['albums'][i]))
-          albumname = data['albums'][i]['title']
-          albumid = data['albums'][i]['id']
-          source = data['albums'][i]['productUrl']
-          break
-      if albumid is None and 'nextPageToken' in data:
-        logging.info('Another page of albums available')
-        params['pageToken'] = data['nextPageToken']
-        continue
-      break
-
-    if albumid is None:
-      url = 'https://photoslibrary.googleapis.com/v1/sharedAlbums'
-      params = {'pageSize':50}#50 is api max
-      while True:
-        data = self.requestUrl(url, params=params)
-        if not data.isSuccess():
-          return None
-        data = json.loads(data.content)
-        if 'sharedAlbums' not in data:
-          logging.debug('User has no shared albums')
-          break
-        for i in range(len(data['sharedAlbums'])):
-          if 'title' in data['sharedAlbums'][i]:
-            logging.debug('Shared Album: %s' % data['sharedAlbums'][i]['title'])
-          if 'title' in data['sharedAlbums'][i] and data['sharedAlbums'][i]['title'].upper().lower().strip() == keyword:
-            albumname = data['sharedAlbums'][i]['title']
-            albumid = data['sharedAlbums'][i]['id']
-            source = data['sharedAlbums'][i]['productUrl']
-            break
-        if albumid is None and 'nextPageToken' in data:
-          logging.info('Another page of shared albums available')
-          params['pageToken'] = data['nextPageToken']
-          continue
-        break
-
-    if albumid is None:
-      return None
-    return {'albumId': albumid, 'sourceUrl' : source, 'albumName' : albumname}
-
-  def selectImageFromAlbum(self, destinationDir, supportedMimeTypes, displaySize, randomize):
+  def selectImageFromAlbum(self, destinationDir, supportedMimeTypes, displaySize, randomize, retry=1):
     result = BaseService.selectImageFromAlbum(self, destinationDir, supportedMimeTypes, displaySize, randomize)
-    if result is not None:
+    if result is None:
+      return None
+    # catch broken urls
+    if result.error is not None and result.source is not None:
+      logging.warning("broken url detected. You should remove '.../%s' from keywords" % (self.getUrlFilename(result.source)))
+    # catch unsupported mimetypes (can only be done after downloading the image)
+    elif result.error is None and result.mimetype not in supportedMimeTypes:
+      logging.warning("unsupported mimetype '%s'. You should remove '.../%s' from keywords" % (result.mimetype, self.getUrlFilename(result.source)))
+    else:
       return result
 
-    if not self.isGooglePhotosEnabled():
-      return BaseService.createImageHolder(self).setError('"Photos Library API" is not enabled on\nhttps://console.developers.google.com\n\nCheck the Photoframe Wiki for details')
-    else:
-      return BaseService.createImageHolder(self).setError('No (new) images could be found.\nCheck spelling or make sure you have added albums')
+    # track broken urls / unsupported mimetypes and display warning message on web interface
+    self.brokenUrls.append(result.source)
+    # retry (with another image)
+    if retry > 0:
+      return self.selectImageFromAlbum(destinationDir, supportedMimeTypes, displaySize, randomize, retry=retry-1)
+    return BaseService.createImageHolder(self).setError('%s uses broken urls / unsupported images!' % self.SERVICE_NAME)
 
   def getImagesFor(self, keyword):
-    filename = os.path.join(self.getStoragePath(), self.hashString(keyword) + '.json')
-    result = []
-    if not os.path.exists(filename):
-      # First time, translate keyword into albumid
-      params = self.getQueryForKeyword(keyword)
-      if params is None:
-        logging.error('Unable to create query the keyword "%s"', keyword)
-        return [BaseService.createImageHolder(self).setError('Unable to get photos using keyword "%s"' % keyword)]
+    url = keyword
+    if url in self.brokenUrls:
+      return []
 
-      url = 'https://photoslibrary.googleapis.com/v1/mediaItems:search'
-      maxItems = 1000 # Should be configurable
+    image_urls = self.getImageUrls(url)
+    images = []
+    for img_url in image_urls:
+        image = BaseService.createImageHolder(self) \
+            .setId(self.hashString(img_url)) \
+            .setUrl(img_url) \
+            .setSource(img_url) \
+            .allowCache(True)
+        images.append(image)
 
-      while len(result) < maxItems:
-        data = self.requestUrl(url, data=params, usePost=True)
-        if not data.isSuccess():
-          logging.warning('Requesting photo failed with status code %d', data.httpcode)
-          logging.warning('More details: ' + repr(data.content))
-          break
-        else:
-          data = json.loads(data.content)
-          if 'mediaItems' not in data:
-            break
-          logging.debug('Got %d entries, adding it to existing %d entries', len(data['mediaItems']), len(result))
-          result += data['mediaItems']
-          if 'nextPageToken' not in data:
-            break
-          params['pageToken'] = data['nextPageToken']
-          logging.debug('Fetching another result-set for this keyword')
+    return images
 
-      if len(result) > 0:
-        with open(filename, 'w') as f:
-          json.dump(result, f)
-      else:
-        logging.error('No result returned for keyword "%s"!', keyword)
-        return []
+#     image = BaseService.createImageHolder(self).setId(self.hashString(url)).setUrl(url).setSource(url).allowCache(True)
+#     return [image]
 
-    # Now try loading
-    if os.path.exists(filename):
-      try:
-        with open(filename, 'r') as f:
-          albumdata = json.load(f)
-      except:
-        logging.exception('Failed to decode JSON file, maybe it was corrupted? Size is %d', os.path.getsize(filename))
-        logging.error('Since file is corrupt, we try to save a copy for later analysis (%s.corrupt)', filename)
-        try:
-          if os.path.exists(filename + '.corrupt'):
-            os.unlink(filename + '.corrupt')
-          os.rename(filename, filename + '.corrupt')
-        except:
-          logging.exception('Failed to save copy of corrupt file, deleting instead')
-          os.unlink(filename)
-        albumdata = None
-    return self.parseAlbumInfo(albumdata)
+  def getImageUrls(self, url):
+    # Fetch the page
+    response = urlopen(url)
+    html = response.read().decode('utf-8', errors='ignore')
+    response.close()
 
-  def parseAlbumInfo(self, data):
-    # parse GooglePhoto specific keys into a format that the base service can understand
-    if data is None:
-      return None
-    parsedImages = []
-    for entry in data:
-      item = BaseService.createImageHolder(self)
-      item.setId(entry['id'])
-      item.setSource(entry['productUrl']).setMimetype(entry['mimeType'])
-      item.setDimensions(entry['mediaMetadata']['width'], entry['mediaMetadata']['height'])
-      if 'description' in entry.keys():
-        item.setDescription(entry['description'])
-      item.allowCache(True)
-      parsedImages.append(item)
-    return parsedImages
+    # Find image sources with regex
+    img_srcs = re.findall(r'<img[^>]+src=["\']?([^"\'>]+)', html, re.IGNORECASE)
+
+    # Convert relative URLs to absolute
+    img_urls = [urljoin(url, src) for src in img_srcs]
+
+    return img_urls
 
   def getContentUrl(self, image, hints):
-    # Tricky, we need to obtain the real URL before doing anything
-    data = self.requestUrl('https://photoslibrary.googleapis.com/v1/mediaItems/%s' % image.id)
-    if data.result != RequestResult.SUCCESS:
-      logging.error('%d,%d: Failed to get URL', data.httpcode, data.result)
-      return None
+    url = image.url
+    url = url.replace('{width}', str(hints['size']['width']))
+    url = url.replace('{height}', str(hints['size']['height']))
+    return url
 
-    data = json.loads(data.content)
-    if 'baseUrl' not in data:
-      logging.error('Data from Google didn\'t contain baseUrl, see original content:')
-      logging.error(repr(data))
-      return None
-    return data['baseUrl'] + "=w" + str(hints['size']["width"]) + "-h" + str(hints['size']["height"])
+  # Treat the entire service as one album
+  # That way you can group images by creating multiple Simple Url Services
+  def nextAlbum(self):
+    # Tell the serviceManager to use next service instead
+    return False
+
+  def prevAlbum(self):
+    # Tell the serviceManager to use previous service instead
+    return False
+
+  def resetToLastAlbum(self):
+    self.resetIndices()
